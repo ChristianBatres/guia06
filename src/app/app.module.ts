1dd0dd8f81f133c4450074f78596340429f2cb68
batres <cbatresaleman97@gmail.com>
2020-09-27 13:50:15 -0600

    actividad 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos.component';
+import { AlumnoListComponent } from './components/alumno-list/alumno-list.component';
+import { AlumnoComponent } from './components/alumno/alumno.component';
+
+//firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
+//Service
+import { AuthService } from "./services/auth.service";
+import { AuthGuard } from "./guard/auth.guard";
+import { AlumnoService } from './services/alumno.service';
+
+// Toastr, para notificaciones en angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    AlumnosComponent,
+    AlumnoListComponent,
+    AlumnoComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthGuard,AlumnoService], //Agregamos a los providers el guard
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDjLb_5ZjJCmdnGhvPvsLtZYCYDuQiDg5k",
+    authDomain: "crudangularfirebase-6f410.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-6f410.firebaseio.com",
+    projectId: "crudangularfirebase-6f410",
+    storageBucket: "crudangularfirebase-6f410.appspot.com",
+    messagingSenderId: "774696953132",
+    appId: "1:774696953132:web:e8f2da58b768e8c770b889",
+    measurementId: "G-FZXGJ8PLRZ"
+  }
 };
 
 /*
